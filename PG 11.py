00a17261ce6f8cad33b19d0bc60a18cf55596649
prethebha21 <108778193+prethebha21@users.noreply.github.com>
2022-07-12 09:47:27 +0530

    Update PG 11.py

--- PG 11.py
+++ PG 11.py
@@ -1,8 +1,6 @@
 rows = int(input("Enter the number of rows: "))
 for i in range(0, rows):
-    # nested loop for each column
     for j in range(0, i + 1):
-        # print star
         print("*", end=' ')
     # new line after each row
     print("\r")
