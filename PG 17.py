e0c979bab0b68681c8b18b1a954abe8a5c16e1e4
prethebha21 <108778193+prethebha21@users.noreply.github.com>
2022-07-12 13:04:36 +0530

    Update PG 17.py

--- PG 17.py
+++ PG 17.py
@@ -7,5 +7,5 @@ def list_swapping(my_list):
 my_list = [34, 21, 56, 78, 93, 20, 11, 9]
 print("The list is :")
 print(my_list)
-print("The list to swap the first and last elements is swapped")
+print("The list after swapping the first and last elements is: ")
 print(list_swapping(my_list))
